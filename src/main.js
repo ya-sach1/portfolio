9dcf12cbbf13c2aea45c8d04f6156451ddb3cc04
ya-sach1 <ikurogane85@gmail.com>
2021-04-25 18:12:05 +0200

    CNAME

--- src/main.js
+++ src/main.js
@@ -4,6 +4,6 @@ import './css/index.css';
 import 'line-awesome/dist/line-awesome/css/line-awesome.min.css';
 
 // ! Javascript
-import './scripts/toggle.js';
+import '../tmp/scripts/toggle.js';
 
-if (['/', '/index.html', '/portfolio', '/portfolio/'].includes(window.location.pathname)) import('./scripts/rotate.js');
+if (['/', '/index.html', '/portfolio', '/portfolio/'].includes(window.location.pathname)) import('../tmp/scripts/rotate.js');
